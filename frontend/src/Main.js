d9443081a2ef7bba6b50f0c9cfa833828655e2a2
Shivangi Nagpal <shivangi_nagpal@Shivangi.local>
2020-04-26 15:32:15 -0700

    admin login

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -7,7 +7,6 @@ import SellerHome from './Components/Seller/sellerHome';
 import CustomerHome from './Components/Customer/customerHome';
 import Customer from './Components/Customer/customer';
 import UserProfile from './Components/User/userProfile';
-import CustomerAddresses from './Components/Customer/customerAddresses';
 //admin
 import adminHome from './Components/Admin/adminHome';
 import inventory from './Components/Admin/inventory';
@@ -19,6 +18,7 @@ import EditEmail from './Components/User/editEmail';
 import SellerOptions from './Components/Seller/sellerOptions';
 import AddNewAddress from './Components/Addresses/addNewAddress';
 import PaymentInfo from './Components/PaymentOptions/paymentInfo';
+import SavedAddresses from './Components/Addresses/savedAddresses';
 
 class Main extends Component {
     render() {
@@ -31,7 +31,6 @@ class Main extends Component {
                     <Route path="/customerHome" component={CustomerHome}/>
                     <Route path="/customer" component={Customer}/>
                     <Route path="/userProfile" component={UserProfile}/>
-                    <Route path="/customerAddresses" component={CustomerAddresses}/>
                     <Route path="/adminHome" component={adminHome} />
                     <Route path="/inventory" component={inventory} />
                     <Route path="/viewSellerList" component={viewSellerList} />       
@@ -41,6 +40,7 @@ class Main extends Component {
                     <Route path="/sellerOptions" component={SellerOptions}/>
                     <Route path="/addNewAddress" component={AddNewAddress}/>
                     <Route path="/paymentInfo" component={PaymentInfo}/>
+                    <Route path="/savedAddresses" component={SavedAddresses}/>
                 </div>
         )
     }
