3a8781764d9e06fc269bfbd8b5b7f9fd07e83916
Prathyusha Lingamallu <prathyusha.lingamallu@sjsu.edu>
2020-04-28 19:57:14 -0700

    adding redis and product rating

--- backend/routes/api/customerRoutes.js
+++ backend/routes/api/customerRoutes.js
@@ -5,11 +5,8 @@ const passport = require('passport');
 var kafka = require('../../kafka/client');
 const passportAuth = passport.authenticate('jwt', { session: false });
 
-router.post("/productSearch",passportAuth, async function (req, res) {
+router.get("/productSearch",passportAuth, async function (req, res) {
  
-   
-   
-  
     console.log("in product search route");
     console.log(req.body);
     kafka.make_request("customer_topic", { "path": "productSearchResults", "user": req.user, "body": req.body }, function (err, results) {
@@ -17,7 +14,7 @@ router.post("/productSearch",passportAuth, async function (req, res) {
       console.log(results);
       console.log(err);
       if (err) {
-        console.log("Inside err");
+        console.log("Inside err REDIS test");
         console.log(err);
         return res.status(err.status).send(err.message);
       } else {
@@ -29,6 +26,53 @@ router.post("/productSearch",passportAuth, async function (req, res) {
         }
       }
     });
+  });
+
+  router.post('/addProductRating', passportAuth, async (req,res)=>{
+    console.log("In add Product Rating");
+    console.log(req.body);
+      kafka.make_request("customer_topic",{"path":"addProductReview", "body": req.body}, function (err, results) {
+        console.log("In make request call back",results);
+        if (err) {
+          console.log("Inside err");
+          console.log(err);
+          return res.status(err.status).send(err.message);
+        } else {
+            console.log("Inside else",results);
+            if (results.status === 200){
+              return res.status(results.status).send(results.data);
+            }else{
+            return res.status(results.status).send(results.errors);
+            }
+        }
+      }
+    );
+  });
+
+  router.post('/addProductReview', passportAuth, async (req,res)=>{
+    console.log("In add Product Review");
+    console.log(req.body);
+      var productReview ={
+        "customerId": req.body.customerId,
+        "comment" : req.body.comment,
+        "rating":req.body.rating
+      }
+      kafka.make_request("customer_topic",{"path":"addProductReview", "body": req.body, "productReview": productReview}, function (err, results) {
+        console.log("In make request call back",results);
+        if (err) {
+          console.log("Inside err");
+          console.log(err);
+          return res.status(err.status).send(err.message);
+        } else {
+            console.log("Inside else",results);
+            if (results.status === 200){
+              return res.status(results.status).send(results.data);
+            }else{
+            return res.status(results.status).send(results.errors);
+            }
+        }
+      }
+    );
   });
   
   module.exports = router;
--- kafka-backend/services/customer_topics.js
+++ kafka-backend/services/customer_topics.js
@@ -1,13 +1,23 @@
 var { ProductCategory } = require('../models/ProductCategory');
 var User = require('../models/User');
 const { prepareSuccess, prepareInternalServerError } = require('./responses');
+const redisClient = require("../utils/redisConfig");
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+
 
 
 exports.customerService = function customerService(msg, callback) {
-    console.log("In customer topic Service path:", msg.path);
+   // console.log("In customer topic Service path:", msg.path);
     switch (msg.path) {
         case "productSearchResults":
             productSearchResults(msg, callback);
+            break;
+        case "addProductReview":
+            addProductReview(msg, callback);
+            break;
+        case "addProductRating":
+            addProductRating(msg, callback);
             break;
 
     }
@@ -80,10 +90,10 @@ function prepareQuery(request, sellerId, product) {
 }
 async function productSearchResults(msg, callback) {
 
-    let response = {};
+     let response = {};
     let result = {};
     let productName = "";
-    console.log("In product search ")
+    console.log("In product search ==============================================================================")
     console.log(msg.body);
 
     var sellerId = null;
@@ -100,16 +110,118 @@ async function productSearchResults(msg, callback) {
         productName = msg.body.search;
     }
 
-           result = await ProductCategory.aggregate(prepareQuery(msg.body, sellerId,productName))
-           .skip(msg.body.pageLimit * (msg.body.currentPage - 1)).limit(msg.body.pageLimit).catch(error => {
-            console.log(error);
-            err = prepareInternalServerError();
+    redisClient.get(msg.body.productCategoryName, async (err, products) => {
+        if (products) {
+
+            products = JSON.parse(products);
+            console.log('REDIS HIT')
+            //response.status = 200;
+             response = prepareSuccess(products);
+            return callback(null, response);
+
+        } else {
+
+            result = await ProductCategory.aggregate(prepareQuery(msg.body, sellerId, productName))
+                .skip(msg.body.pageLimit * (msg.body.currentPage - 1)).limit(msg.body.pageLimit).catch(error => {
+                    console.log(error);
+                    err = prepareInternalServerError();
+                    return callback(err, null);
+                })
+            redisClient.setex(msg.body.productCategoryName, 36000, JSON.stringify(result), function (error, reply) {
+                if (error) {
+                    console.log(error);
+                }
+            });
+            response = prepareSuccess(result);
+            console.log(response);
+            return callback(null, response);
+        }
+    })
+
+}
+
+async function addProductRating(msg, callback) {
+    let response = {};
+    let err = {};
+    await ProductCategory.findOne({ "products._id": msg.body._id })
+        .select('products')
+        .then(async result => {
+            if (result.products.productReview) {
+                db.users.aggregate([
+                    { count: { $size: '$result.products.productReview.CustomerId' } }
+                ])
+                await ProductCategory.findOneAndUpdate(({ "products._id": msg.body._id },
+                    { $set: { "products.productRating": result.products.productReview.rating / count } }))
+                    .then(result1 => {
+                        response.data = result1;
+                        response.status = 200;
+                        return callback(null, response);
+                    }).catch(error => {
+                        console.log(error);
+                        err.status = 410;
+                        err.message = "could not add product rating to product";
+                        err.data = error;
+                        return callback(err, null);
+                    })
+            }
+        });
+}
+
+
+async function addProductReview(msg, callback) {
+    let response = {};
+    let err = {};
+    let query = [];
+    console.log("In seller topic service. Msg: ", msg);
+    var review = {
+        customerId: msg.body.customerId,
+        comment: msg.body.comment,
+        rating: msg.body.rating,
+    };
+    query.push(
+        {
+            $unwind: "$products"
+        },
+        {
+            $match: { "products._id": ObjectId(msg.body._id) }
+        },
+        {
+            $unwind: "$products.productReview"
+        },
+        {
+            $count: "customers"
+        }
+
+
+    )
+    console.log(query);
+    let result = await ProductCategory.aggregate(query)
+
+
+    var { customers } = result;
+    console.log(customers);
+    console.log(result);
+
+    let avgrating = msg.body.rating;
+    if (result.length > 0) {
+        avgrating = ((msg.body.productRating * result[0].customers) + msg.body.rating) / (result[0].customers + 1);
+        console.log(avgrating);
+    }
+    await ProductCategory.updateOne(
+        { "products._id": msg.body._id },
+        {
+            'products.$.productRating': avgrating,
+            $addToSet: { 'products.$.productReview': review }
+        },
+        { new: true })
+        .then((result) => {
+            response = prepareSuccess(result);
+            return callback(null, response);
+        }).catch(error => {
+            err = prepareInternalServerError(error);
             return callback(err, null);
         })
 
-    response = prepareSuccess(result);
-    console.log(response);
-    return callback(null, response);
 
 
 }
