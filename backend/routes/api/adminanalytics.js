72784b8f72121ab6c4caf64ddc7c6f9bbd35cafe
suryakatari97 <surya.katari@sjsu.edu>
2020-05-06 20:02:54 -0700

    admin analytics

--- backend/routes/api/adminanalytics.js
+++ backend/routes/api/adminanalytics.js
@@ -6,6 +6,7 @@ const passportAuth = passport.authenticate('jwt', { session: false });
 const Order = require('../../models/Order');
 const Seller = require('../../models/Seller');
 const User = require('../../models/User');
+var { ProductCategory } = require('../../models/ProductCategory');
 
 router.get("/noOfOrdersPerDay",  (req , res) => {
     console.log("IN NOOF ORDERS PER DAY");
@@ -44,16 +45,69 @@ router.get("/orderStatusAdminGraph", (req, res) => {
 })
 
 router.get("/top5Customers",(req,res) => {
-    
-
+    Order.aggregate([
+        {
+            $group:{
+                _id: "$customerId",
+                amount: {
+                    $sum: "$totalAmount"
+                }
+            }
+        },{
+            $sort:{"amount":-1}
+        }
+    ]).limit(5)
+    .then(result => {
+        res.end(JSON.stringify(result));
+    }).catch(err => {
+        res.end("could not get messages");
+    })
 });
 
-router.get("/top5Sellers",(req,res) => {
+router.get("/top10ProductsBasedOnRating",(req,res) => {
+    ProductCategory.aggregate([
+        {
+            $unwind: "$products"
+        },{
+            $project:{
+                "productName": "$products.productName",
+                "productRating": "$products.productRating"
+            }
+        },{
+            $sort:{"productRating":-1}
+        }
+    ]).limit(10)
+        .then(result => {
+            res.end(JSON.stringify(result));
+        }).catch(err => {
+            res.end("could not get messages");
+        })
+})
 
+//can optimize the query
+router.get("/top5Sellers",(req,res) => {
+    Seller.aggregate([
+        {
+        $group:{
+            _id:"$seller",
+            amount: {
+                $sum: "$salesTotal"
+            }
+        }
+    },{
+            $sort: {"amount":-1}
+        }
+    ]).limit(5)
+        .then(result => {
+            res.end(JSON.stringify(result));
+        }).catch(err => {
+            res.end("could not get messages");
+        })
 });
 router.get("/top5SoldProducts", (req, res) => {
     console.log("IN top5SoldProducts ");
     Order.aggregate([
+        
         {
             $unwind: "$products"
         },{
@@ -65,13 +119,12 @@ router.get("/top5SoldProducts", (req, res) => {
             }
         },{
             $sort:{"count":-1}
-        }   
+        }
+        
         ]).limit(5)
         .then(result => {
-            console.log("messages retreived", result);
             res.end(JSON.stringify(result));
         }).catch(err => {
-            console.log(err);
             res.end("could not get messages");
         })
 })
---
+++ frontend/src/Components/Admin/Top5Sellers.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Bar } from 'react-chartjs-2';
+import axios from 'axios';
+
+ class Top5Sellers extends Component {
+     constructor(props) {
+         super(props);
+         this.state = {
+             sellers: [],
+             count: []
+         }
+     }
+
+     componentDidMount() {
+         axios.get("/top5Sellers")
+             .then((response) => {
+                 console.log("THIS IS RESPONSE", response);
+                 let sellersArr = [];
+                 let valArr = [];
+                 response.data.forEach((item) => {
+                     sellersArr.push(item._id);
+                     valArr.push(item.amount)
+                 });
+                 this.setState({
+                     sellers: sellersArr,
+                     count: valArr
+                 });
+             })
+     }
+
+    render() {
+        const data = {
+            labels: this.state.sellers,
+            datasets: [
+                {
+                    label: 'Top 5 Sellers.',
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: this.state.count
+                }
+            ]
+        }
+        return (
+            <div>
+                <div style={{ background: "#fafafa" }}>
+                    <Bar ref="chart" data={data}
+                        options={{
+                            maintainAspectRatio: true,
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true,
+                                        stepSize: 100
+                                    }
+                                }]
+                            }, title: {
+                                display: true,
+                                text: 'Top 5 Sellers'
+                            }
+                        }} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Top5Sellers;
